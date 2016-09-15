00c3892c9babde99302bb976775cce88fefea45f
Pravar Mahajan <mahajan.89@osu.edu>
2016-09-15 15:30:58 -0400

    parser updated to output json

--- README.txt
+++
@@ -1,4 +0,0 @@
-In parser script , I tested for 2 sample .sgm file with 2 reuters tag in each file. 
-You can change the path to point to reuters folder and then run it. 
-There is slight issue in it. I am storing each tag like places, topic etc in list. There is separate lists for places and topics as they can be many. These two lists are not updating correctly. You will find the topics and places or first reuters tag is repeated everywhere. 
-This thing need to be changed. Other than this. I think it will run fine in cse servers. 
--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import os
 import glob
 import json
+import pdb
 
 path = "data/"
 result = []
@@ -9,35 +10,36 @@ for infile in glob.glob(os.path.join(path, "*.sgm")):
 	f = open(infile,'r')
 	soup = BeautifulSoup(f, 'html.parser')
 	soup.prettify()
-
-	tit = None
-	top = []
-	plc = []
-	bd = None
-
+	
+	title = None
+	topics = []
+	places = []
+	body = None
+	data = {}
 	for reuters in soup.find_all('reuters'):
-		tit = None
-		del top[:] 
-		del plc[:] 
-		bd = None
+		title = None
+		del topics[:] 
+		del places[:] 
+		body = None
+		data.clear()
 		if reuters.title != None:
-			tit = reuters.title.string
+			title = reuters.title.string
+			data["title"] = title
 		if reuters.topics != None:
-			for tp in soup.reuters.topics.find_all('d'):
-				top.append(tp.string) 
+			for topic in reuters.topics.find_all('d'):
+				topics.append(topic.string) 
+				data["topics"] = list(topics)
 		if reuters.places != None:
-			for pl in soup.reuters.places.find_all('d'):
-				plc.append(pl.string) 
+			for place in reuters.places.find_all('d'):
+				places.append(place.string)
+				data["places"] = list(places)
 		if reuters.body != None:
-			bd = reuters.body.string
-		result.append((tit, top, plc, bd))
+			body = reuters.body.string.replace('\n', '')
+			data["body"] = body
 		
+		result.append(data.copy())
 
-myfile = open(path+"output.txt", "w")
+myfile = open(path + "output.txt", "w")
 json.dump(result, myfile, indent=4, separators=(',', ':'))
 
 del result[:]
-
-
-    	
-
