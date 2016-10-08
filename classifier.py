6870510720b983b4d096c0a7c450fcfe819d3f99
anu10 <yadav.anu843@gmail.com>
2016-10-08 13:46:55 -0400

    adding comments

--- classifier.py
+++ classifier.py
@@ -17,7 +17,30 @@ import preprocessing_config
 import text2vec
 import time
 
-'''Perform an 80-20 split on the input data. Output is a tuple in this
+
+''' Filter out topics_labels and tf_idf_matrix points where topics_labels is not empty.'''
+def filter_topics():
+    my_tf_idf_matrix = text2vec.load_sparse_matrix_from_file("tf_idf_matrix")
+    my_tf_idf_matrix = my_tf_idf_matrix.tocsr()
+    with open(os.path.join(preprocessing_config.output_data_dir, "topics_labels.dat"), 'r') as f:
+        my_topics_labels = f.readlines()
+
+    a = []
+    rows_to_keep = []
+    idx = 0
+    for label in my_topics_labels:
+        label = label.strip()
+        if label != "":
+            a.append(label)
+            rows_to_keep.append(idx)
+        idx += 1
+
+    X = my_tf_idf_matrix[rows_to_keep, :]
+    Y = np.array([s.split(',') for s in a])
+    return X, Y
+
+
+'''Perform an 80-20 split on the filtered data. Output is a tuple in this
 format: (X_train, X_test, Y_train, Y_test). The problem here is that the labels
 have only one or two instances, because of which they appear only in the
 training set or only in the test set. We will randomly split the data 80-20
@@ -55,6 +78,10 @@ def split_data_80_20(train_data, train_labels):
 
     return X_train, Y_train, X_test, Y_test
 
+''' MultiLabelBinarizer transform between iterable of iterables and a multilabel format. 
+Although a list of sets or tuples is a very intuitive format for multilabel data, it is unwieldy to process.
+This transformer converts between this intuitive format and the supported multilabel format: 
+a (samples x classes) binary matrix indicating the presence of a class label.'''
 def binarize_labels(labels, binarizer=None):
     if binarizer == None:
         binarizer = MultiLabelBinarizer()
@@ -99,36 +126,16 @@ def test_knn_classifier(clf, test_data, test_labels):
 
     return Y_pred
 
-def filter_topics():
-    my_tf_idf_matrix = text2vec.load_sparse_matrix_from_file("tf_idf_matrix")
-    my_tf_idf_matrix = my_tf_idf_matrix.tocsr()
-    with open(os.path.join(preprocessing_config.output_data_dir, "topics_labels.dat"), 'r') as f:
-        my_topics_labels = f.readlines()
-
-    a = []
-    rows_to_keep = []
-    idx = 0
-    for label in my_topics_labels:
-        label = label.strip()
-        if label != "":
-            a.append(label)
-            rows_to_keep.append(idx)
-        idx += 1
-
-    X = my_tf_idf_matrix[rows_to_keep, :]
-    Y = np.array([s.split(',') for s in a])
-    return X, Y
-
 '''Perform decision tree classification on the input dataset. Prints the time to train the data'''
 def train_decision_tree_classifier(train_data, train_labels):
     time1 = time.time()
-    clf  = tree.DecisionTreeClassifier()
+    clf  = tree.DecisionTreeClassifier(random_state=0)
 
     clf.fit(train_data, train_labels)
     time2 = time.time()
     print "Time to train = %.2f secs" %(time2-time1)
-    print "Best params obtained:"
-    print clf.feature_importances_
+    #print "Best params obtained:"
+    #print clf.feature_importances_
     return clf
 
 '''Tests the test data using training data. It compares current test_label with the observed labels. Accuracy, Precision, Recall and Time to test are also computed. '''
